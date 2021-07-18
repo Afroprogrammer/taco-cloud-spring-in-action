0e33c0fd704c45e546fa654bdbd56f026346b588
Franklin <forsonfranklin@gmail.com>
2021-07-18 22:24:05 +0000

    Initial commit

---
+++ src/main/java/com/example/tacocloudspringinaction/TacoCloudSpringInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudspringinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudSpringInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudSpringInActionApplication.class, args);
+    }
+
+}
